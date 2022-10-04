d14aff36f97c62b51f6864c85ece3eb9a893a7f7
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-04 19:28:11 +0200

    Everything published at mondotondo.com/goldbach

---
+++ checkframes.js
@@ -0,0 +1,3 @@
+if( !( parent && parent.parent && parent.parent.xgc ) ) {
+	document.writeln( 'you should see this in <a href="http://www.mondotondo.com/aercolino/goldbach/index.html" target="_blank">the XGC website</a>' );
+}
