e2216569647e2a633277d21b56550f71a8df4227
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-08 21:33:32 +0200

    Add types, and make ESLint happy

--- vite-project/src/playground/Page.ts
+++ vite-project/src/playground/Page.ts
@@ -1,92 +1,62 @@
 import { xgc_maxFactorable, XGC_EuclidSet, xgc_Divides, XGC_Partition } from './XGC';
 
-const styles = {
+const styles: Record<string, string> = {
   Euclid: "border: thin dashed #C0C0C0; margin-top: 3pt; padding: 2pt 5pt 2pt 30pt; text-indent: -25pt",
   Partition: "border: 1pt ridge; margin-top: 3pt; padding: 2pt 5pt 2pt 30pt; text-indent: -25pt",
   Partition_Element: "",
   Partition_Group: "border: 1pt ridge; margin-top: 3pt; padding: 2pt 5pt 2pt 30pt; text-indent: -25pt"
 };
 
-let f;
+let f: HTMLFormElement;
 document.addEventListener('DOMContentLoaded', () => {
-  f = document.inputForm;
+  f = document.forms.inputForm as HTMLFormElement;
 });
 
-const euclidArray = [];
-let thisResidue;
-let thisModulus;
-let thisTerms;
-let thisEuclidSet;
-let thisNumber;
-let thisNumberTo;
-let thisCount;
+const euclidArray: XGC_EuclidSet[] = [];
+let thisResidue: number;
+let thisModulus: number;
+let thisTerms: number;
+let thisEuclidSet = -1;
+let thisNumber: number;
+let thisNumberTo: number;
+let thisCount: number;
 let lastID = -1;
 
-window.Page = {};
-
-window.Page.ShowInfo = function ShowInfo() {
-  window.open(
-    "xgc_js_info.html",
-    "",
-    "directory=no, menubar=no, location=no, scrollbars=yes, resizable=yes, toolbar=yes, width=600, height=400"
-  );
-}
-
-window.Page.outputClear = function outputClear() {
-  parent.outputFrame.document.location = "xgc_js_output.html";
-}
-
-function makeDiv(divClass, divId, divContent) {
+function makeDiv(divClass: string, divId: string, divContent: string) {
   const divStyle = ' style="' + styles[divClass] + '"';
   divId = ' id="' + divId + '"';
   return "<div" + divId + divStyle + ">" + divContent + "</div>";
 }
 
-function outputWrite(html, gotoId) {
-  const doc = parent.outputFrame.document;
+function outputWrite(html: string, gotoId: string) {
+  const doc = (parent.outputFrame as Window).document;
   doc.writeln(html);
   doc.location = "#" + gotoId;
 }
 
 function EuclidSetID() {
-  return "e" + thisEuclidSet;
+  return `e${thisEuclidSet}`;
 }
 
 function LastID() {
   return ++lastID;
 }
 
-function EuclidSetHeader(thisResidue, thisModulus, thisTerms) {
-  return (
-    "<b>Euclid( " +
-    thisResidue +
-    ", " +
-    thisModulus +
-    " )</b> |<sub>" +
-    thisTerms +
-    "</sub>"
-  );
+function EuclidSetHeader(thisResidue: number, thisModulus: number, thisTerms: number) {
+  return `<b>Euclid(${thisResidue}, ${thisModulus})</b> |<sub>${thisTerms}</sub>`;
 }
 
 function addOption() {
-  const select = f.EuclidSets;
+  const select = f.EuclidSets as HTMLSelectElement;
   thisEuclidSet = select.options.length;
-  const label =
-    thisEuclidSet +
-    ":" +
-    "    " +
-    euclidArray[thisEuclidSet].residue +
-    "    " +
-    euclidArray[thisEuclidSet].modulus +
-    "    (" +
-    euclidArray[thisEuclidSet].terms +
-    ")";
+  const set = euclidArray[thisEuclidSet];
+  const label = `${thisEuclidSet}: Euclid(${set.residue}, ${set.modulus}) | ${set.terms}`;
   select.options[thisEuclidSet] = new Option(label, thisEuclidSet);
   select.options[thisEuclidSet].selected = true;
 }
 
 function getSelectedOption() {
-  const select = f.EuclidSets;
+  const select = f.EuclidSets as HTMLSelectElement;
   for (let i = 0; i < select.options.length; i++)
     if (select.options[i].selected) return select.options[i];
   return false;
@@ -97,27 +67,13 @@ function getCurrentIndex() {
   return selected == false ? false : parseInt(selected.value);
 }
 
-window.Page.copyCurrent = function copyCurrent() {
-  thisEuclidSet = getCurrentIndex();
-  //  readFields();
-  if (thisEuclidSet === false) return false;
-  outputEuclidSet(euclidArray[thisEuclidSet], LastID());
-}
-
-window.Page.findCurrent = function findCurrent() {
-  thisEuclidSet = getCurrentIndex();
-  //  readFields();
-  if (thisEuclidSet === false) return false;
-  parent.outputFrame.document.location = "#e" + thisEuclidSet;
-}
-
-function outputEuclidSet(euclidSet, id) {
+function outputEuclidSet(euclidSet: XGC_EuclidSet, id: string) {
   const header = EuclidSetHeader(
     euclidSet.residue,
     euclidSet.modulus,
     euclidSet.terms
   );
-  const body = "" + euclidSet.values;
+  const body = `${String(euclidSet.values)}`;
   outputWrite(makeDiv("Euclid", id, header + " = " + body), id);
 }
 
@@ -128,106 +84,129 @@ function readFields() {
     thisModulus = euclidArray[thisEuclidSet].modulus;
     thisTerms = euclidArray[thisEuclidSet].terms;
   }
-  thisNumber = parseInt(f.inputNumber.value);
-  thisNumberTo = parseInt(f.inputNumberTo.value);
-  thisCount = parseInt(f.inputCount.value);
-}
-
-window.Page.EuclidSetCompute = function EuclidSetCompute() {
-  const thisResidue = parseInt(f.inputResidue.value);
-  const thisModulus = parseInt(f.inputModulus.value);
-  const thisTerms = parseInt(f.inputTerms.value);
-
-  if (thisResidue + thisModulus * thisTerms < xgc_maxFactorable) {
-    const euclidSet = new XGC_EuclidSet(
-      thisResidue,
-      thisModulus,
-      thisTerms
-    );
-    euclidArray[euclidArray.length] = euclidSet;
-    addOption();
-    adjustFields("inputPartition");
-    outputEuclidSet(euclidSet, EuclidSetID());
-  } else {
-    alert("Incorrect Euclid Set parameters.");
-  }
+  thisNumber = parseInt((f.inputNumber as HTMLInputElement).value);
+  thisNumberTo = parseInt((f.inputNumberTo as HTMLInputElement).value);
+  thisCount = parseInt((f.inputCount as HTMLInputElement).value);
 }
 
 function usingClause() {
-  return (
-    ' &nbsp;&nbsp; <nobr><font size="-1">using&nbsp;<a href="#' +
-    EuclidSetID() +
-    '">' +
-    EuclidSetHeader(thisResidue, thisModulus, thisTerms) +
-    "</a></font></nobr>"
-  );
+  return ` &nbsp;&nbsp; <nobr><font size="-1">using&nbsp;<a href="#${EuclidSetID()}">${EuclidSetHeader(thisResidue, thisModulus, thisTerms)}</a></font></nobr>`;
 }
 
-window.Page.PartitionLoop = function PartitionLoop() {
-  adjustFields("inputLoop");
-  readFields();
 
-  if (
-    xgc_Divides(thisModulus, thisNumber) &&
-    euclidArray[thisEuclidSet].values
-  ) {
-    const partition = new XGC_Partition(euclidArray[thisEuclidSet]);
-    const id = LastID();
-    const uClause = usingClause();
-    let html = "";
-    for (
-      let number = thisNumber;
-      number <= thisNumberTo;
-      number += thisModulus
-    ) {
-      const html2 =
-        "<b>" + number + "</b> = +" + partition.get(number) + uClause;
-      html += makeDiv("Partition_Element", "", html2);
+const Page = {
+  ShowInfo() {
+    window.open(
+      "xgc_js_info.html",
+      "",
+      "directory=no, menubar=no, location=no, scrollbars=yes, resizable=yes, toolbar=yes, width=600, height=400"
+    );
+  },
+
+  outputClear() {
+    (parent.outputFrame as Window).document.location = "xgc_js_output.html";
+  },
+
+  copyCurrent() {
+    thisEuclidSet = getCurrentIndex();
+    //  readFields();
+    if (thisEuclidSet === false) return false;
+    outputEuclidSet(euclidArray[thisEuclidSet], LastID());
+  },
+
+  findCurrent() {
+    thisEuclidSet = getCurrentIndex();
+    //  readFields();
+    if (thisEuclidSet === false) return false;
+    (parent.outputFrame as Window).document.location = `#${EuclidSetID()}`;
+  },
+
+  EuclidSetCompute() {
+    const thisResidue = parseInt((f.inputResidue as HTMLInputElement).value);
+    const thisModulus = parseInt((f.inputModulus as HTMLInputElement).value);
+    const thisTerms = parseInt((f.inputTerms as HTMLInputElement).value);
+  
+    if (thisResidue + thisModulus * thisTerms < xgc_maxFactorable) {
+      const euclidSet = new XGC_EuclidSet(
+        thisResidue,
+        thisModulus,
+        thisTerms
+      );
+      euclidArray[euclidArray.length] = euclidSet;
+      addOption();
+      this.adjustFields("inputPartition");
+      outputEuclidSet(euclidSet, EuclidSetID());
+    } else {
+      alert("Incorrect Euclid Set parameters.");
     }
+  },
 
-    outputWrite(makeDiv("Partition_Group", id, html), id);
-  } else {
-    alert("Incorrect Partition parameters.");
+  PartitionLoop() {
+    this.adjustFields("inputLoop");
+    readFields();
+  
+    if (
+      xgc_Divides(thisModulus, thisNumber) &&
+      euclidArray[thisEuclidSet].values
+    ) {
+      const partition = new XGC_Partition(euclidArray[thisEuclidSet]);
+      const id = LastID();
+      const uClause = usingClause();
+      let html = "";
+      for (
+        let number = thisNumber;
+        number <= thisNumberTo;
+        number += thisModulus
+      ) {
+        const html2 = `<b>${number}</b> = +${String(partition.get(number))}${uClause}`;
+        html += makeDiv("Partition_Element", "", html2);
+      }
+  
+      outputWrite(makeDiv("Partition_Group", id, html), id);
+    } else {
+      alert("Incorrect Partition parameters.");
+    }
+  },
+
+  PartitionCompute() {
+    readFields();
+  
+    if (
+      xgc_Divides(thisModulus, thisNumber) &&
+      euclidArray[thisEuclidSet].values
+    ) {
+      const partition = new XGC_Partition(euclidArray[thisEuclidSet]);
+      const id = LastID();
+      const uClause = usingClause();
+      const number = thisNumber;
+      const html2 = `<b>${number}</b> = +${String(partition.get(number))}${uClause}`;
+  
+      outputWrite(makeDiv("Partition", id, html2), id);
+    } else {
+      alert("Incorrect Partition parameters.");
+    }
+  },
+
+  adjustFields(choice) {
+    readFields();
+    switch (choice) {
+      case "inputCount":
+        (f.inputCount as HTMLInputElement).value = (thisNumberTo - thisNumber) / thisModulus + 1;
+        break;
+      case "inputLoop":
+        (f.inputCount as HTMLInputElement).value = thisCount <= 0 ? 5 : thisCount;
+        (f.inputNumberTo as HTMLInputElement).value = thisNumber + thisModulus * (thisCount - 1);
+        break;
+      case "inputNumberTo":
+        (f.inputNumberTo as HTMLInputElement).value = (thisCount - 1) * thisModulus + thisNumber;
+        break;
+      case "inputPartition":
+        (f.inputNumber as HTMLInputElement).value = (thisResidue + thisModulus) * thisModulus;
+        (f.inputCount as HTMLInputElement).value = thisCount <= 0 ? 5 : thisCount;
+        (f.inputNumberTo as HTMLInputElement).value = thisNumber + thisModulus * (thisCount - 1);
+        break;
+    }
   }
-}
+};
 
-window.Page.PartitionCompute = function PartitionCompute() {
-  readFields();
-
-  if (
-    xgc_Divides(thisModulus, thisNumber) &&
-    euclidArray[thisEuclidSet].values
-  ) {
-    const partition = new XGC_Partition(euclidArray[thisEuclidSet]);
-    const id = LastID();
-    const uClause = usingClause();
-    const number = thisNumber;
-    const html2 =
-      "<b>" + number + "</b> = +" + partition.get(number) + uClause;
-
-    outputWrite(makeDiv("Partition", id, html2), id);
-  } else {
-    alert("Incorrect Partition parameters.");
-  }
-}
-
-const adjustFields = window.Page.adjustFields = function adjustFields(choice) {
-  readFields();
-  switch (choice) {
-    case "inputCount":
-      f.inputCount.value = (thisNumberTo - thisNumber) / thisModulus + 1;
-      break;
-    case "inputLoop":
-      f.inputCount.value = thisCount <= 0 ? 5 : thisCount;
-      f.inputNumberTo.value = thisNumber + thisModulus * (thisCount - 1);
-      break;
-    case "inputNumberTo":
-      f.inputNumberTo.value = (thisCount - 1) * thisModulus + thisNumber;
-      break;
-    case "inputPartition":
-      f.inputNumber.value = (thisResidue + thisModulus) * thisModulus;
-      f.inputCount.value = thisCount <= 0 ? 5 : thisCount;
-      f.inputNumberTo.value = thisNumber + thisModulus * (thisCount - 1);
-      break;
-  }
-}
+window.Page = Page;
