b9dd721d820f8ace8972f0e4e21cbec8667f26d2
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-10 19:11:43 +0200

    Don't swallow errors

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin({
+      dev: true
+    }),
+    eslint()
+  ],
 });
