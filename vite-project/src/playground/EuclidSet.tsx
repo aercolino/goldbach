743535a27cd05ce95c946469165af58527e79e6c
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-11 19:01:09 +0200

    Simplify code

--- vite-project/src/playground/EuclidSet.tsx
+++ vite-project/src/playground/EuclidSet.tsx
@@ -22,13 +22,12 @@ export function EuclidSet() {
     (modulusValue || modulusNumber()) * (limitValue || limitNumber());
 
   const [validClass, setValidClass] = createSignal("");
-  const isPrimeToModulus = (num: number) => xgc_IsPrimeTo(num, modulusNumber());
   const validateClass = (field: HTMLInputElement) => {
     const value = Math.abs(Math.floor(field.valueAsNumber));
     let error = "";
     if (!(1 <= value && value <= modulusNumber() - 1))
       error = `${value} is out of range`;
-    if (!isPrimeToModulus(value))
+    if (!xgc_IsPrimeTo(value, modulusNumber()))
       error = `${value} is not prime to ${modulusNumber()}`;
     const max = maxSievedNumber({ classValue: value });
     if (!(max <= xgc_maxFactorable)) error = `${max} > ${xgc_maxFactorable}`;
@@ -59,80 +58,78 @@ export function EuclidSet() {
   };
 
   return (
-    <>
-      <SimpleGrid columns={4} columnGap="20px" rowGap="10px">
-        <Box bg="$neutral3">
-          <NumberField
-            id="class"
-            label={
-              <>
-                Class <code>C</code>
-              </>
-            }
-            min="1"
-            max={modulusNumber() - 1}
-            helper={
-              <>
-                An integer such that <code>0 &lt; C &lt; M</code>,{" "}
-                <code>GCD(C, M) = 1</code>
-              </>
-            }
-            value={classNumber}
-            setValue={setClassNumber}
-            validate={validateClass}
-            error={validClass}
-          />
-        </Box>
-        <Box bg="$neutral3">
-          <NumberField
-            id="modulus"
-            label={
-              <>
-                Modulus <code>M</code>
-              </>
-            }
-            min="2"
-            helper={
-              <>
-                An integer greater than <code>2</code>
-              </>
-            }
-            value={modulusNumber}
-            setValue={setModulusNumber}
-            validate={validateModulus}
-            error={validModulus}
-          />
-        </Box>
-        <Box bg="$neutral3">
-          <NumberField
-            id="limit"
-            label={
-              <>
-                Limit <code>L</code>
-              </>
-            }
-            min="1"
-            helper={
-              <>
-                An integer greater than <code>0</code>. The last sieved number
-                is <code>C + M * L = {maxSievedNumber()}</code>
-              </>
-            }
-            value={limitNumber}
-            setValue={setLimitNumber}
-            validate={validateLimit}
-            error={validLimit}
-          />
-        </Box>
-        <Center bg="$neutral3">
-          <VStack>
-            <Button colorScheme="primary">Sieve Numbers</Button>
-            <Text size="xs" color="$blackAlpha8" mt="$4">
-              the max factorable number is {xgc_maxFactorable}
-            </Text>
-          </VStack>
-        </Center>
-      </SimpleGrid>
-    </>
+    <SimpleGrid columns={4} columnGap="20px" rowGap="10px">
+      <Box bg="$neutral3">
+        <NumberField
+          id="class"
+          label={
+            <>
+              Class <code>C</code>
+            </>
+          }
+          min="1"
+          max={modulusNumber() - 1}
+          helper={
+            <>
+              An integer such that <code>0 &lt; C &lt; M</code>,{" "}
+              <code>GCD(C, M) = 1</code>
+            </>
+          }
+          value={classNumber}
+          setValue={setClassNumber}
+          validate={validateClass}
+          error={validClass}
+        />
+      </Box>
+      <Box bg="$neutral3">
+        <NumberField
+          id="modulus"
+          label={
+            <>
+              Modulus <code>M</code>
+            </>
+          }
+          min="2"
+          helper={
+            <>
+              An integer greater than <code>2</code>
+            </>
+          }
+          value={modulusNumber}
+          setValue={setModulusNumber}
+          validate={validateModulus}
+          error={validModulus}
+        />
+      </Box>
+      <Box bg="$neutral3">
+        <NumberField
+          id="limit"
+          label={
+            <>
+              Limit <code>L</code>
+            </>
+          }
+          min="1"
+          helper={
+            <>
+              An integer greater than <code>0</code>. The last sieved number is{" "}
+              <code>C + M * L = {maxSievedNumber()}</code>
+            </>
+          }
+          value={limitNumber}
+          setValue={setLimitNumber}
+          validate={validateLimit}
+          error={validLimit}
+        />
+      </Box>
+      <Center bg="$neutral3">
+        <VStack>
+          <Button colorScheme="primary">Sieve Numbers</Button>
+          <Text size="xs" color="$blackAlpha8" mt="$4">
+            the max factorable number is {xgc_maxFactorable}
+          </Text>
+        </VStack>
+      </Center>
+    </SimpleGrid>
   );
 }
