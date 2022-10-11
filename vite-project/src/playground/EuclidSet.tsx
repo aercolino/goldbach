afedf06a283b048386bc2db857d4fbe9c3637fcb
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-11 07:16:28 +0200

    Add fields class, modulus, and limit with basic dependencies

--- vite-project/src/playground/EuclidSet.tsx
+++ vite-project/src/playground/EuclidSet.tsx
@@ -1,3 +1,89 @@
+import {
+  SimpleGrid,
+  Box,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+} from "@hope-ui/solid";
+import { createSignal, createMemo } from "solid-js";
+
 export function EuclidSet() {
-  return <div>EUCLID SET</div>
+  const [classNumber, setClassNumber] = createSignal(1);
+  const [modulusNumber, setModulusNumber] = createSignal(2);
+  const [limitNumber, setLimitNumber] = createSignal(30);
+  const maxSievedNumber = createMemo(
+    () => classNumber() + modulusNumber() * limitNumber()
+  );
+
+  return (
+    <>
+      <SimpleGrid columns={4} columnGap="20px" rowGap="10px">
+        <Box bg="$neutral3">
+          <FormControl>
+            <FormLabel for="class" width="$full">
+              Class <code>C</code>
+            </FormLabel>
+            <Input
+              id="class"
+              type="number"
+              width="$3xs"
+              value={classNumber()}
+              min="1"
+              max={modulusNumber() - 1}
+              onChange={(e) =>
+                setClassNumber(parseInt((e.target as HTMLInputElement).value))
+              }
+            />
+            <FormHelperText>
+              An integer such that <code>0 &lt; C &lt; M</code>, and coprime
+              with <code>M</code>
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box bg="$neutral3">
+          <FormControl>
+            <FormLabel for="modulus" width="$full">
+              Modulus <code>M</code>
+            </FormLabel>
+            <Input
+              id="modulus"
+              type="number"
+              width="$3xs"
+              value={modulusNumber()}
+              onChange={(e) =>
+                setModulusNumber(parseInt((e.target as HTMLInputElement).value))
+              }
+              min="2"
+            />
+            <FormHelperText>
+              An integer greater than <code>1</code>
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box bg="$neutral3">
+          <FormControl>
+            <FormLabel for="limit" width="$full">
+              Limit <code>L</code>
+            </FormLabel>
+            <Input
+              id="limit"
+              type="number"
+              width="$3xs"
+              value={limitNumber()}
+              onChange={(e) =>
+                setLimitNumber(parseInt((e.target as HTMLInputElement).value))
+              }
+              min="1"
+            />
+            <FormHelperText>
+              An integer greater than <code>0</code>. The last sieved number is{" "}
+              <code>C + M * L = {maxSievedNumber()}</code>
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box bg="$neutral3" />
+      </SimpleGrid>
+    </>
+  );
 }
