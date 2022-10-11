d849f2e00a76663b9e7ec8747b766fb0abd9d5b3
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-11 14:04:27 +0200

    Use navy forr the title bar color

--- vite-project/src/playground/Playground.tsx
+++ vite-project/src/playground/Playground.tsx
@@ -1,14 +1,15 @@
 import { hope, Heading } from "@hope-ui/solid";
-import { EuclidSet } from './EuclidSet';
-import { Partition } from './Partition';
-import { Dashboard } from './Dashboard';
-
+import { EuclidSet } from "./EuclidSet";
+import { Partition } from "./Partition";
+import { Dashboard } from "./Dashboard";
 
 export function Playground() {
   return (
     <>
-      <hope.div bgColor="$primary9" p="$2">
-        <Heading level="2" size="2xl" color="$primary1">Playground</Heading>
+      <hope.div bgColor="navy" p="$2">
+        <Heading level="2" size="2xl" color="$primary1">
+          Playground
+        </Heading>
       </hope.div>
       <EuclidSet />
       <Partition />
