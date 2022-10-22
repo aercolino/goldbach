ee91a837b67338d06e6818967dec7af9fd4851f4
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-10-22 17:10:55 +0200

    Start to develop a Range component

--- vite-project/src/playground/Range.tsx
+++ vite-project/src/playground/Range.tsx
@@ -1,41 +1,121 @@
 import { hope } from "@hope-ui/solid";
+import { createRenderEffect, createSignal } from "solid-js";
 
-export function Range() {
+type SliderPropsType = {
+  min: number;
+  max: number;
+  value: () => number;
+  setValue: (a: number) => number;
+  width: number;
+};
+
+function SliderMin(props: SliderPropsType) {
+  let sliderMin, sliderMax, sliderValue, sliderWidth;
+  createRenderEffect(() => {
+    const min = props.min ?? 0;
+    const max = props.max ?? 100;
+    let value = props.value();
+    if (!(min <= value && value <= max)) {
+      value = min;
+    }
+    sliderMin = -100;
+    sliderMax = 0;
+    sliderValue = (-100 * value) / (max - min);
+    sliderWidth = (sliderValue / 100) * props.width;
+  });
   return (
-    <hope.div style={{ position: "relative" }}>
+    <hope.div>
       <hope.input
-        w="160px"
+        w={sliderWidth}
         type="range"
-        min={-100}
-        max={0}
-        value={-20}
+        min={sliderMin}
+        max={sliderMax}
+        value={sliderValue}
         style={{
           direction: "rtl",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          border: "none",
-          outline: "none",
-          height: "2px",
-        }}
-      />
-      <hope.input
-        w="160px"
-        ml="40px"
-        type="range"
-        min={0}
-        max={100}
-        value={80}
-        style={{
-          direction: "ltr",
-          position: "absolute",
-          top: 0,
-          left: 0,
           border: "none",
           outline: "none",
           height: "2px",
         }}
       />
     </hope.div>
+  );
+}
+
+function SliderMax(props: SliderPropsType) {
+  let sliderMin, sliderMax, sliderValue, sliderWidth, sliderLeftMargin;
+  createRenderEffect(() => {
+    const min = props.min ?? 0;
+    const max = props.max ?? 100;
+    let value = props.value();
+    if (!(min <= value && value <= max)) {
+      value = max;
+    }
+    sliderMin = 0;
+    sliderMax = 100;
+    sliderValue = (100 * value) / (max - min);
+    sliderLeftMargin = (sliderValue / 100) * props.width;
+    sliderWidth = props.width - sliderLeftMargin;
+  });
+
+  return (
+    <hope.div>
+      <hope.input
+        w={sliderWidth}
+        ml={sliderLeftMargin}
+        type="range"
+        min={sliderMin}
+        max={sliderMax}
+        value={sliderValue}
+        style={{
+          direction: "ltr",
+          border: "none",
+          outline: "none",
+          height: "2px",
+        }}
+      />
+    </hope.div>
+  );
+}
+
+type RangePropsType = {
+  width: number;
+  min: number;
+  max: number;
+  minValue?: number;
+  maxValue?: number;
+};
+
+export function Range(props: RangePropsType) {
+  let minValue, setMinValue, maxValue, setMaxValue;
+  createRenderEffect(() => {
+    // eslint-disable-next-line solid/reactivity
+    [minValue, setMinValue] = createSignal(props.minValue ?? props.min);
+    // eslint-disable-next-line solid/reactivity
+    [maxValue, setMaxValue] = createSignal(props.maxValue ?? props.max);
+  });
+
+  return (
+    <>
+      <hope.div style={{ position: "relative" }}>
+        <SliderMin
+          min={props.min}
+          max={props.max}
+          value={minValue}
+          setValue={setMinValue}
+          width={props.width}
+        />
+        <SliderMax
+          min={props.min}
+          max={props.max}
+          value={maxValue}
+          setValue={setMaxValue}
+          width={props.width}
+        />
+      </hope.div>
+      <hope.div>
+        Min: {minValue()} - Max: {maxValue()}
+      </hope.div>
+    </>
   );
 }
