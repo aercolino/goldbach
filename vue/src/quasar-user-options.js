3c8a80e14c282574d9c7e67e7bf018576078e5c6
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-08-19 10:46:16 +0200

    Rename `vite-project` folder to `solid-js`

---
+++ vue/src/quasar-user-options.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+  return {
+    supportTS: false, // Set this to true if you want to use TypeScript
+    boot: [],
+    css: [],
+    extras: [],
+    build: {},
+    devServer: {},
+  };
+};
