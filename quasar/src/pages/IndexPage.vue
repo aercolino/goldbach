161b105ce765c094735d92d7ef3c780dcee373da
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-09-04 21:22:01 +0200

    Add a sample slider for the modulus

--- quasar/src/pages/IndexPage.vue
+++ quasar/src/pages/IndexPage.vue
@@ -1,13 +1,38 @@
 <template>
   <q-page class="flex flex-center">
-    XGC Playground
+    <div class="q-pa-md" style="width: 1000px">
+    <q-badge color="secondary">
+      Model: {{ value }} (0 to 100)
+    </q-badge>
+
+    <q-slider
+      v-model="value"
+      :min="0"
+      :max="100"
+      :step="1"
+      :inner-min="2"
+      snap
+      label-always
+      markers
+      :marker-labels="objMarkerLabels"
+      color="purple"
+    />
+  </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  name: 'IndexPage',
+  setup () {
+    const markers = new Array(101).fill().map((x, val) => (val === 1 || val % 5 === 0) ? val : null).filter(x => x)
+
+    return {
+      value: ref(2),
+      objMarkerLabels: Object.fromEntries(markers.map(x => [x,x])),
+    }
+  },
+}
 </script>
