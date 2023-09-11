620eb33570b2fe60d131ff32edea2fc0a442a3e4
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-09-11 12:51:31 +0200

    Improve display

--- quasar/src/components/MultiplesRange.vue
+++ quasar/src/components/MultiplesRange.vue
@@ -1,11 +1,15 @@
 <template>
-  [{{ EuclidSetsStore.selected.m * selectedSet.at(0) }},
-  {{ EuclidSetsStore.selected.m * selectedSet.at(-1) }}]
+  multiples of {{ EuclidSetsStore.selected.m }}, from {{ min }} to {{ max }}
+  <CardinalityBadge :count="cardinality" />
 </template>
 
 <script setup>
 import { computed } from "vue"
+import CardinalityBadge from "./CardinalityBadge.vue"
 import { useEuclidSetsStore } from "src/stores/EuclidSets"
 const EuclidSetsStore = useEuclidSetsStore()
-const selectedSet = computed(() => EuclidSetsStore.getSelected?.getValues() || [])
+const selectedSet = computed(() => EuclidSetsStore.getSelected)
+const min = computed(() => EuclidSetsStore.selected.m * selectedSet.value?.at(0))
+const max = computed(() => EuclidSetsStore.selected.m * selectedSet.value?.at(-1))
+const cardinality = computed(() => (max.value - min.value) / EuclidSetsStore.selected.m + 1)
 </script>
