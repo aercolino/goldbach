00dd13d68be0f7cc008b6a16d674d7de73d3c719
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-09-17 18:12:10 +0200

    Add `FailuresSetTitle` component

---
+++ quasar/src/components/FailuresSetTitle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="col text-body1">
+    {{ (store.selected.m === 2 && "Goldbach Conjecture") || "Extended Goldbach Conjecture" }}
+    failures
+  </div>
+</template>
+
+<script setup>
+import { useEuclidSetsStore } from "src/stores/EuclidSets"
+const store = useEuclidSetsStore()
+</script>
