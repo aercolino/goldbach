f23a8b044bdb04153fc9185fdb9c5884e2493eb6
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-09-25 11:58:10 +0200

    Improve EuclidSet tests

--- quasar/src/maths/__tests__/EuclidSet.mjs
+++ quasar/src/maths/__tests__/EuclidSet.mjs
@@ -2,7 +2,10 @@ import { XGC_EuclidSet } from "../XGC.mjs"
 
 describe("XGC_EuclidSet", () => {
   it("should work", () => {
-    const xgc = new XGC_EuclidSet(22, 71, 150)
-    expect(xgc.values.values.length).toBe(33)
+    const EuclidSet_1_2_150 = new XGC_EuclidSet(1, 2, 150)
+    expect(EuclidSet_1_2_150.values.values.length).toBe(61)
+
+    const EuclidSet_14_15_150 = new XGC_EuclidSet(14, 15, 150)
+    expect(EuclidSet_14_15_150.values.values.length).toBe(45)
   })
 })
