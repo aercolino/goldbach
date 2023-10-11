ef8581fbeaa414da87b46f4c3e6c545d7aee2ed1
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-10-11 08:46:31 +0200

    Add a `List` proxy of Array, meant to replace XGC_Array

---
+++ quasar/src/maths/List.mjs
@@ -0,0 +1,117 @@
+export function List(arg) {
+  let data
+  let methods
+  let methodsNames
+  initialize(arg)
+
+  return new Proxy(data, {
+    get(target, prop, receiver) {
+      if (prop === "isList") {
+        return true
+      }
+      const { value: index, isNumber } = toNumber(prop)
+      if (isNumber && (index < 1 || target.length < index)) return undefined
+      if (isNumber) {
+        return getData(index)
+      }
+      if (methodsNames.includes(prop)) {
+        return methods[prop]
+      }
+      if (typeof Array.prototype[prop] === "function") {
+        // To simplify, we just pass through all Array methods down to the target
+        // array, even if some of them are 0 based
+        return target[prop].bind(target)
+      }
+      return Reflect.get(target, prop, receiver)
+    },
+    set(target, prop, value, receiver) {
+      const { value: index, isNumber } = toNumber(prop)
+      if (isNumber && (index < 1 || target.length < index))
+        throw new Error(`Expected an existing index. Got "${index}".`)
+      if (isNumber) {
+        return setData(index, value)
+      }
+      return Reflect.set(target, prop, value, receiver)
+    },
+  })
+
+  function getData(index) {
+    return data[index - 1]
+  }
+
+  function setData(index, value) {
+    data[index - 1] = value
+    return true
+  }
+
+  function toNumber(x) {
+    const number = parseInt(x, 10)
+    return {
+      isNumber: !Number.isNaN(number),
+      value: number,
+    }
+  }
+
+  function initData(arg) {
+    if (!arg) {
+      data = []
+    } else if (Array.isArray(arg)) {
+      data = arg.slice()
+    }
+    if (data) return
+
+    const { value, isNumber } = toNumber(arg)
+    if (isNumber && value < 0)
+      throw new Error(`Expected a length argument. Got "${JSON.stringify(arg)}".`)
+
+    if (isNumber) {
+      data = new Array(arg).fill(0)
+    } else if (arg.isList) {
+      data = arg.toArray()
+    } else throw new Error(`Expected a numerical or list argument. Got "${JSON.stringify(arg)}".`)
+  }
+
+  function initMethods() {
+    methods = {
+      toArray() {
+        return data.slice()
+      },
+      toJSON() {
+        return data
+      },
+      getChoice(selection) {
+        return List(selection.map((x) => getData(x)))
+      },
+      sumChoice(selection) {
+        return selection.reduce((acc, x) => (acc += getData(x)), 0)
+      },
+      findIndex(value) {
+        let lowIndex = 1
+        let highIndex = data.length
+        let foundIndex = 0
+        let notFoundIndex = 0
+        while (lowIndex <= highIndex) {
+          foundIndex = Math.floor((lowIndex + highIndex) / 2)
+          const middleValue = getData(foundIndex)
+          if (middleValue === value) {
+            return foundIndex
+          } else if (middleValue > value) {
+            notFoundIndex = foundIndex - 1
+            highIndex = foundIndex - 1
+          } else {
+            notFoundIndex = foundIndex
+            lowIndex = foundIndex + 1
+          }
+        }
+        // getData(notFoundIndex) < value < getData(notFoundIndex + 1)
+        return notFoundIndex
+      },
+    }
+    methodsNames = Object.keys(methods)
+  }
+
+  function initialize(arg) {
+    initData(arg)
+    initMethods()
+  }
+}
---
+++ quasar/src/maths/__tests__/List.mjs
@@ -0,0 +1,123 @@
+import { List } from "../List.mjs"
+
+describe("List", () => {
+  describe("constructor", () => {
+    it("should work with no arguments", () => {
+      const list = List()
+      // BREAKING CHANGE: `new XGC_Array()` produced `[0]`
+      expect(list.toArray()).toEqual([])
+    })
+    it("should work with an integer argument", () => {
+      const list = List(5)
+      expect(list.toArray()).toEqual([0, 0, 0, 0, 0])
+    })
+    it("should work with an array argument", () => {
+      const start = [1, 2, 3]
+      const list = List(start)
+      expect(list.toArray()).toEqual([1, 2, 3])
+      expect(list).not.toBe(start)
+
+      expect(List([1]).toArray()).toEqual([1])
+    })
+    it("should work with a list argument", () => {
+      const start = [1, 2, 3]
+      const list1 = List(start)
+      const list2 = List(list1)
+      expect(list2.toArray()).toEqual([1, 2, 3])
+      expect(list2).not.toBe(list1)
+
+      expect(List([1]).toArray()).toEqual([1])
+    })
+    it("should throw if anything but nothing, a length, an array, or a list is passed as an argument", () => {
+      expect(() => List(-2)).toThrow()
+    })
+  })
+
+  describe("get value at index", () => {
+    it("should return undefined if index is less than 1 or greater than the length of values", () => {
+      const list1 = List()
+      expect(list1[0]).toBe(undefined)
+      expect(list1[-10]).toBe(undefined)
+      expect(list1[2]).toBe(undefined)
+      expect(list1[+10]).toBe(undefined)
+    })
+
+    it("should return the value at `index-1`", () => {
+      const list1 = List()
+      expect(list1.toArray()).toEqual([])
+      expect(list1[1]).toBe(undefined)
+      expect(list1.toArray()).toEqual([])
+
+      const list2 = List(3)
+      expect(list2.toArray()).toEqual([0, 0, 0])
+      expect(list2[2]).toBe(0)
+      expect(list2.toArray()).toEqual([0, 0, 0])
+
+      const list3 = List([1, 2, 3])
+      expect(list3.toArray()).toEqual([1, 2, 3])
+      expect(list3[2]).toBe(2)
+      expect(list3.toArray()).toEqual([1, 2, 3])
+    })
+  })
+
+  describe("set value at index", () => {
+    it("should throw if index is less than 1 or greater than the length of values", () => {
+      const list1 = List()
+      expect(() => {
+        list1[0] = 4
+      }).toThrow()
+      expect(() => {
+        list1[-10] = 4
+      }).toThrow()
+      expect(() => {
+        list1[2] = 4
+      }).toThrow()
+      expect(() => {
+        list1[+10] = 4
+      }).toThrow()
+    })
+
+    it("should set the value at `index-1`", () => {
+      const list2 = List(3)
+      expect(list2.toArray()).toEqual([0, 0, 0])
+      list2[2] = 4
+      expect(list2.toArray()).toEqual([0, 4, 0])
+
+      const list3 = List([1, 2, 3])
+      expect(list3.toArray()).toEqual([1, 2, 3])
+      list3[2] = 4
+      expect(list3.toArray()).toEqual([1, 4, 3])
+    })
+  })
+
+  describe(".getChoice(selection)", () => {
+    it("should select values at given indexes", () => {
+      const list1 = List([2, 4, 6, 8, 10])
+      expect(list1.getChoice(List([2, 4])).toArray()).toEqual([4, 8])
+    })
+  })
+
+  describe(".sumChoice(selection)", () => {
+    it("should add up values at given positions", () => {
+      const list1 = List([2, 4, 6, 8, 10])
+      expect(list1.sumChoice(List([1, 3, 5]))).toBe(18)
+    })
+  })
+
+  describe(".findIndex()", () => {
+    it("should work with an empty list", () => {
+      expect(List([]).findIndex(3)).toBe(0)
+    })
+    it("should work with a list of 1 item", () => {
+      expect(List([3]).findIndex(3)).toBe(1)
+      expect(List([3]).findIndex(2)).toBe(0)
+      expect(List([3]).findIndex(4)).toBe(1)
+    })
+    it("should work with a list of 2 items", () => {
+      expect(List([3, 5]).findIndex(2)).toBe(0)
+      expect(List([3, 5]).findIndex(3)).toBe(1)
+      expect(List([3, 5]).findIndex(4)).toBe(1)
+      expect(List([3, 5]).findIndex(6)).toBe(2)
+    })
+  })
+})
