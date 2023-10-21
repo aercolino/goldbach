a0b0ef0e7a36d422d8bd775917a5b8872386250c
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-10-21 14:53:59 +0200

    Show number of failures

--- quasar/src/components/PercentageSummary.vue
+++ quasar/src/components/PercentageSummary.vue
@@ -1,9 +1,13 @@
 <template>
-  <BaseSummary> failures / multiples = {{ store.percentage }} % </BaseSummary>
+  <BaseSummary>
+    {{ FailuresSet.length }} failures / {{ store.multiples }} multiples = {{ store.percentage }} %
+  </BaseSummary>
 </template>
 
 <script setup>
 import BaseSummary from "./BaseSummary.vue"
+import { computed } from "vue"
 import { useEuclidSetsStore } from "src/stores/EuclidSets"
 const store = useEuclidSetsStore()
+const FailuresSet = computed(() => store.getSelected[1])
 </script>
