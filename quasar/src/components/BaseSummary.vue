309ab316c1e52ba40c72dd7d1be0d162e7dd84f7
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2023-10-21 18:01:27 +0200

    Use a lighter color for summaries

--- quasar/src/components/BaseSummary.vue
+++ quasar/src/components/BaseSummary.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="col text-body2">
+  <div class="col text-body2" :style="{ color: textColor }">
     <slot></slot>
   </div>
 </template>
+
+<script setup>
+import { colors } from "quasar"
+const { getPaletteColor } = colors
+const textColor = getPaletteColor("brown-6")
+</script>
